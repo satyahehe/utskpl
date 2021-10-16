259cf82862fc8a94e1db5d17fd108dfa301016a9
farhanrizqussalam <76400195+farhanrizqussalam@users.noreply.github.com>
2021-10-16 16:33:09 +0700

    Update delete_customer.php

--- bookorama/delete_customer.php
+++ bookorama/delete_customer.php
@@ -7,7 +7,7 @@ $query = "DELETE FROM customers WHERE customerid = ".$customerid."";
 //execute query
 $result = $db->query($query);
 if (!$result) {
-    die("Could not query the database: <br/>" . $db->error . '<br>Query' . $query);
+    ("Could not query the database: <br/>" . $db->error . '<br>Query' . $query);
 } else {
     $db->close();
     header('Location: ../bookorama/view_customer.php');
